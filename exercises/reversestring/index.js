c9da2e2917d7139cd9ddf63bac8951105d3dd0e1
NaebIis <johnmlowery@gmail.com>
2019-11-12 12:20:55 -0600

    reverseString done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,41 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {};
 module.exports = reverse;
+
+// function reverse(str) {
+//   let newStr = str;
+//   newStr.split('');
+//   let temp = '';
+//   for (let character of newStr) {
+//     temp = character + temp;
+//   };
+//   newStr = temp;
+//   return newStr
+// };
+
+// function reverse(str) {
+//   const newStr = str.psplit('').reverse().join('');
+//   return newStr;
+// };
+
+// function reverse(str) {
+//   let newStr = str;
+//   newStr.split('');
+//   let temp = [];
+//   for (let i = newStr.length; i >= 0; i--) {
+//     temp.push(newStr[i]);
+//   };
+//   newStr = temp.join('');
+//   return newStr
+// };
+
+// function reverse(str) {
+//   let newStr = str.split('');
+//   return newStr.reduce((temp, character) => {
+//     return character + temp;
+//   }, '');
+// };
+
+
