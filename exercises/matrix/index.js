5d67c1ee6ba355a7b467a7a9a92351406d720e44
Naeblis <johnmlowery@gmail.com>
2020-01-30 12:05:49 -0600

    spiral matrix complete

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+
+  let startcolumn = n - 1;
+  let startrow = 0;
+  let endcolumn = 0;
+  let endrow = n - 1;
+  let counter = 1;
+  let container = [];
+  
+  while (container.length <= n - 1) {
+    container.push([]);
+  };
+
+  while (counter <= n ** 2) {
+    for (let i = endcolumn; i <= startcolumn; i++) {
+      container[startrow][i] = counter;
+      counter++;
+    } startrow++;
+
+    for (let i = startrow; i <= endrow; i++) {
+      container[i][startcolumn] = counter;
+      counter++;
+    } startcolumn--;
+
+    for (let i = startcolumn; i >= endcolumn; i--) {
+      container[endrow][i] = counter
+      counter++
+    } endrow--
+
+    for (let i = endrow; i >= startrow; i--) {
+      container[i][endcolumn] = counter
+      counter++
+    } endcolumn++
+  }
+  return container;
+}
+
+console.log(matrix(3));
+console.log(matrix(5));
+console.log(matrix(6));
+console.log(matrix(15));
 
 module.exports = matrix;
